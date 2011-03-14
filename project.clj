f57f82662e721883a24f4bbe464c7bc89e124290
Tobias Raeder <tobias.raeder@freiheit.com>
2011-03-14 12:55:09 +0100

    Added :main to project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject slick-test "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [slick "1.0.0"]
-                 [org.lwjgl/lwjgl "2.7.1"]
-                 [org.lwjgl/lwjgl-util "2.7.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [native-deps "1.0.5"]]
+  :description         "FIXME: write"
+  :dependencies        [[org.clojure/clojure "1.2.0"]
+                        [org.clojure/clojure-contrib "1.2.0"]
+                        [slick "1.0.0"]
+                        [org.lwjgl/lwjgl "2.7.1"]
+                        [org.lwjgl/lwjgl-util "2.7.1"]]
+  :dev-dependencies    [[swank-clojure "1.2.1"]
+                        [native-deps "1.0.5"]]
+  :main                slick-test.core
   :native-dependencies [[org.lwjgl/lwjgl-native-platform "2.7.1"]])
