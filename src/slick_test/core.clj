e76a4d231c9bfa23af49741c0c641a29aaeda1a8
Pirmin Fix <pirmin@42degreesoffreedom.com>
2011-03-15 22:41:56 +0100

    Added plane and movement
    Known bugs:
     - plane dissappears regularily. Whyever
     - Plane doesn't move in y-direction. Whyever

--- src/slick_test/core.clj
+++ src/slick_test/core.clj
@@ -1,19 +1,41 @@
 (ns slick-test.core
-  (:import [org.newdawn.slick AppGameContainer BasicGame GameContainer Graphics Image SlickException])
-  (:gen-class))
-
-(defn make-game
-  [window-name image]
-  (let [img (atom nil)]
-    (proxy [BasicGame] [window-name]
+  (:gen-class)
+  (:import [org.newdawn.slick AppGameContainer BasicGame GameContainer Graphics Image Input SlickException]))
+(defn to-radians [degrees] (Math/toRadians degrees))
+(defn sinus-of [rad] (Math/sin rad))
+(defn cosin-of [rad] (Math/cos rad))
+(def SimpleGame
+  (let
+    [land (atom nil)
+     plane (atom nil)
+     plane-data (atom [400 300])]
+    (proxy
+      [BasicGame]
+      ["Slick2DPath2Glory - SimpleGame"]
       (init [gc]
-        (swap! img (fn [img] (Image. (str "resources/" image)))))
+        (let [img (fn [x file] (Image. file))]
+          (swap! land
+                 img "land.jpg")
+          (swap! plane
+                 img "plane.png")))
       (update [gc delta]
-        (.draw @img 100 100))
-      (render [gc graphics]))))
-
-(defn -main
-  []
-  (doto (AppGameContainer. (make-game "lol pirm" "pirmin.png"))
+        (let [input (.getInput gc)
+              key-down? (fn [key] (.isKeyDown input key))]
+            (if (key-down? Input/KEY_A)
+              (.rotate @plane (* -0.2 delta)))
+            (if (key-down? Input/KEY_D)
+              (.rotate @plane (* 0.2 delta)))
+            (if (key-down? Input/KEY_W)
+              (swap! plane-data
+                     (fn [pd hip rotation]
+                       [(+ (* hip (sinus-of rotation)) (first pd))
+                        (- (* hip (cosin-of rotation)) (last pd))])
+                     (* 0.4 delta)
+                     (to-radians (.getRotation @plane))))))
+      (render [gc g]
+        (.draw @land 0 0)
+        (apply #(.drawCentered %1 %2 %3) @plane @plane-data)))))
+(defn -main []
+  (doto (AppGameContainer. SimpleGame)
     (.setDisplayMode 800 600 false)
     (.start)))
