98244b9502b3cba2ac9dceeb9d74347ad1f980e5
Pirmin Fix <pirmin@42degreesoffreedom.com>
2011-03-20 18:52:22 +0100

    a more clojure style (hopefully) variant

--- src/slick_test/core.clj
+++ src/slick_test/core.clj
@@ -1,43 +1,26 @@
 (ns slick-test.core
   (:gen-class)
   (:use [clojure.contrib.generic.math-functions :only [sin cos]])
-  (:import [org.newdawn.slick AppGameContainer BasicGame GameContainer Graphics Image Input SlickException]))
+  (:import [org.newdawn.slick
+	    AppGameContainer BasicGame GameContainer Graphics Image Input SlickException]))
 
-(def SimpleGame
-  (let
-    [land (atom nil)
-     plane (atom nil)
-     plane-data (atom [400 300])]
-    (proxy [BasicGame]
-      ["Slick2DPath2Glory - SimpleGame"]
+(def land (atom nil))
+(defn create-image [storage file]
+  (Image. file))
 
-      (init [gc]
-        (let [img (fn [x file] (Image. file))]
-          (swap! land
-                 img "land.jpg")
-          (swap! plane
-                 img "plane.png")))
+(defn init-game [gc])
+(defn update-game [gc delta])
+(defn render-game [gc g])
 
-      (update [gc delta]
-        (let [input (.getInput gc)
-              key-down? (fn [key] (.isKeyDown input key))]
-            (if (key-down? Input/KEY_A)
-              (.rotate @plane (* -0.2 delta)))
-            (if (key-down? Input/KEY_D)
-              (.rotate @plane (* 0.2 delta)))
-            (if (key-down? Input/KEY_W)
-              (swap! plane-data
-                     (fn [pd hip rotation]
-                       [(+ (first pd) (* hip (sin rotation)))
-                        (- (peek pd) (* hip (cos rotation)))])
-                     (* 0.4 delta)
-                     (Math/toRadians (.getRotation @plane))))))
+(def simple-game
+     (proxy [BasicGame]
+	 ["Slick2DPath2Glory - SimpleGame"]
+       (init [gc]
+	     (init-game gc))
+       (update [gc delta]
+	       (update-game gc delta))
+       (render [gc g]
+	       (render-game gc g))))
 
-      (render [gc g]
-        (.draw @land 0 0)
-        (apply #(.drawCentered %1 %2 %3) @plane @plane-data)))))
+(def app-game-container (AppGameContainer. simple-game))
 
-(defn -main []
-  (doto (AppGameContainer. SimpleGame)
-    (.setDisplayMode 800 600 false)
-    (.start)))
